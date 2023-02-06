7e6661199f55a2a9423514288f8dd24995792151
Judah Agnew <judah.agnew121@gmail.com>
2023-02-06 13:12:40 -0800

    created app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState,useEffect } from 'react';
+import MusicTable from './Component/MusicTable/MusicTable';
+import AddEntry from './Component/AddEntry/AddEntry';
+import './App.css'
+import SearchBar from './Component/SearchBar/SearchBar';
+
 
 function App() {
+
+    const [song, setSongs] = useState([{title:'', artist: '', genre:'', release_date:'', likes:'' }])
+
+    useEffect(() => {
+      getAllSongs();
+    }, [])
+
+    async function getAllSongs(){
+      try{
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        console.log(response.data)
+        setSongs(response.data);
+
+      } catch(ex) {
+        console.log ('Error');
+      }
+    }
+  
+  async function addSong(newSong){
+    let response = await axios.post('http://127.0.0.1:8000/music/', newSong);
+    if(response.status === 201){
+      await getAllSongs();
+} 
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+        <SearchBar songs={song} setSongs={setSongs} />
+        <button className ='getAllSongs' onClick = {getAllSongs}>Music Time Download your Library!</button>
+      </div>
+      <div className='row-posts'>
+        <MusicTable parentMusicTable = {song}/>
+      </div>  
+      <div className='row-createSong'>
+        <AddEntry addNewSongProperty = {addSong}/>
+        </div>
+      
     </div>
-  );
+    );
+  
 }
 
 export default App;
